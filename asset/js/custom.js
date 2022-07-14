e31b408c820ba970407854be54bcd25c49622d54
KDS <dasomm57@naver.com>
2022-07-14 21:00:07 +0900

    수정5

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -28,4 +28,27 @@ $(function(){
     scrollbar.addListener(function (status) { //Scrollbar 쓰는 순간 스크롤 이벤트는 못씀 
         ScrollTrigger.refresh();
     });
+
+
+
+
+
+    const leftMotion = gsap.timeline({})
+    leftMotion.addLabel('label')
+     .to('.msg-box .msg1',{yPercent:-35,duration:0.1},'label')
+    .to('.msg-box .msg2',{yPercent:-35,duration:0.1},'label')
+    .to('.msg-box .msg3',{yPercent:-35,duration:0.1},'label')
+
+    .fromTo('.msg-box',{
+        yPercent:100,
+        opacity:0,
+        
+    },{
+        yPercent:0,
+        opacity:1,
+        
+    })
+
+
+
 })
